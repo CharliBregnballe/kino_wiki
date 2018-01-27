60936cca81f5068d8af156f96a837d2f8c648522
Charli <charli@kompjuter.dk>
2018-01-27 13:48:32 +0100

    Added test for empty title and body and added validations on article model

--- spec/models/article_spec.rb
+++ spec/models/article_spec.rb
@@ -1,5 +1,17 @@
 require 'rails_helper'
 
 RSpec.describe Article, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  describe Article do 
+    it "is not possible to create an article without a title" do 
+      a = Article.new(title: "", body:"Some random body text")
+      a.save
+      expect(Article.count).to eq(0)
+    end
+
+    it "is not possible to create an article without a body" do 
+      a = Article.new(title: "Some randome title", body:"")
+      a.save
+      expect(Article.count).to eq(0)
+    end
+  end
 end

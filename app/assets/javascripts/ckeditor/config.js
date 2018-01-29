8e46a212fef268ec459c278b15f545c8b191876f
Charli <charli@kompjuter.dk>
2018-01-29 14:10:08 +0100

    Removed browser url scripts in the ckeditor config file

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -6,14 +6,7 @@ CKEDITOR.editorConfig = function (config) {
   ];
   config.toolbar = "simple";
 
-  
-  config.filebrowserBrowseUrl           = "/ckeditor/attachment_files"
-  config.filebrowserFlashBrowseUrl      = "/ckeditor/attachment_files"
-  config.filebrowserFlashUploadUrl      = "/ckeditor/attachment_files"
-  config.filebrowserImageBrowseLinkUrl  = "/ckeditor/pictures"
-  config.filebrowserImageBrowseUrl      = "/ckeditor/pictures"
-  config.filebrowserImageUploadUrl      = "/ckeditor/pictures"
-  config.filebrowserUploadUrl           = "/ckeditor/attachment_files"
+
 
   // ... rest of the original config.js  ...
 }

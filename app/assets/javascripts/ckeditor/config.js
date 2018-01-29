65985d6b200a9e53aa6218d6017f5995a546e9a0
Charli <charli@kompjuter.dk>
2018-01-29 12:44:00 +0100

    Added some configuration to ckeditors config.js to be able to upload images

--- app/assets/javascripts/ckeditor/config.js
+++ app/assets/javascripts/ckeditor/config.js
@@ -4,7 +4,16 @@ CKEDITOR.editorConfig = function (config) {
   config.toolbar_mini = [
     ["Bold",  "Italic",  "Underline",  "Strike",  "-",  "Subscript",  "Superscript"],
   ];
-  config.toolbar = "advanced";
+  config.toolbar = "simple";
+
+  
+  config.filebrowserBrowseUrl           = "/ckeditor/attachment_files"
+  config.filebrowserFlashBrowseUrl      = "/ckeditor/attachment_files"
+  config.filebrowserFlashUploadUrl      = "/ckeditor/attachment_files"
+  config.filebrowserImageBrowseLinkUrl  = "/ckeditor/pictures"
+  config.filebrowserImageBrowseUrl      = "/ckeditor/pictures"
+  config.filebrowserImageUploadUrl      = "/ckeditor/pictures"
+  config.filebrowserUploadUrl           = "/ckeditor/attachment_files"
 
   // ... rest of the original config.js  ...
 }

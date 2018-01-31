4061fb3ab3315db99f7967cfaac69cdbe6aab494
Charli <charli@kompjuter.dk>
2018-01-31 15:31:17 +0100

    Added association between users and departments

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180130230020) do
+ActiveRecord::Schema.define(version: 20180131142602) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -56,8 +56,11 @@ ActiveRecord::Schema.define(version: 20180130230020) do
     t.datetime "updated_at", null: false
     t.string "first_name"
     t.string "last_name"
+    t.bigint "department_id"
+    t.index ["department_id"], name: "index_users_on_department_id"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
+  add_foreign_key "users", "departments"
 end

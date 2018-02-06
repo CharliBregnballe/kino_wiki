bf650b8fcd4ddfb154f74a7e79fa9dcb5bb37356
Charli <charli@kompjuter.dk>
2018-02-06 22:25:37 +0100

    Added authentication method as a before action in the application controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users, :controllers => { :registrations => "registrations" }
   mount Ckeditor::Engine => '/ckeditor'
   resources :articles
-  root 'articles#index'
+
+  authenticated :user do
+    root to: 'articles#index', as: :authenticated_root
+  end
+  root to: redirect('/users/sign_in')
 end

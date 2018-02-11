b4833749623a3cf994e0dee8815a3b25a8eb29bf
Charli <charli@kompjuter.dk>
2018-02-11 14:18:26 +0100

    Added redirect from www to https in application controllr

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,6 +4,14 @@ class ApplicationController < ActionController::Base
   before_action :configure_permitted_parameters, if:  :devise_controller?
   before_action :authenticate_user! 
 
+  before_filter :redirect_subdomain
+
+  def redirect_subdomain
+    if request.host == 'www.wiki.kino.dk'
+      redirect_to 'https://wiki.kino.dk' + request.full_path, :status => 301
+    end
+  end
+
   protected
 
     def configure_permitted_parameters
